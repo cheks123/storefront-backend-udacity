d6f765eaffb01a5f12fa5795c5d878cc6314ca81
Chekwube Utomi <59573850+cheks123@users.noreply.github.com>
2023-01-30 16:23:15 +0100

    Delete migrations directory

--- migrations/sqls/20230119073859-users-up.sql
+++
@@ -1,9 +0,0 @@
-/* Replace with your SQL commands */
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    username VARCHAR(60) NOT NULL UNIQUE,
-    password VARCHAR NOT NULL
-);
